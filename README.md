cd5dd1b1ba4fd2cc3635e48679caeeaa55fb1fd3
Daniel Ockeloen <daniel@noterik.nl>
2014-07-17 22:00:06 +0200

    added new gui, licenses and added readme.

---
+++ README.md
@@ -0,0 +1,12 @@
+EUScreen preview app
+====================
+
+This app is part of the EUScreen project, it has very little value to outside developers except as a example of code. Its a Html5Application
+as defined inside the Multiscreen Tookit and should be deployed as such (check the application manager wiki)
+
+1) Check out smt_euscreenxlpreview in Eclipse
+2) Build a war using the 'deploy-war' task with the provided build.xml
+3) Do not deploy it in tomcat!, we use the war format but it should be loaded in Lou so use the application manager
+
+** unless you are working on the EUScreen project, probably not much use for this application since you are missing the needed
+data inside smithers. But it can be useful for code examples of how todo things with the toolkit so use it as such.
--- src/org/springfield/lou/application/types/EuscreenxlpreviewApplication.java
+++ src/org/springfield/lou/application/types/EuscreenxlpreviewApplication.java
@@ -4,19 +4,20 @@
 * Copyright (c) 2012 Noterik B.V.
 * 
 * This file is part of Lou, related to the Noterik Springfield project.
+* It was created as part of the EUScreen/EUScreen project (www.euscreen.eu)
 *
-* Lou is free software: you can redistribute it and/or modify
+* EUScreen Preview app is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* Lou is distributed in the hope that it will be useful,
+* EUScreen Preview app is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU General Public License
-* along with Lou.  If not, see <http://www.gnu.org/licenses/>.
+* along with EUScreen Preview app.  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.springfield.lou.application.types;
 
